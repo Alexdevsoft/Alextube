6442e001e4de5ae7ceb763e2f4e21cfc9befc7a4
Alexdevsoft <alexhavilla2022@gmail.com>
2022-11-10 08:31:08 -0300

    Atualização tube

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,36 @@
+import React from "react";
 import config from "../config.json";
 import styled from "styled-components";
 import { CSSReset } from "../src/componets/CSSReset";
 import Menu from "../src/componets/Menu";
-import TimeLine from "../src/componets/TimeLine";
+import { StyledTimeline } from "../src/componets/Timeline";
 
-export default function HomePage() {
-    const estilosDaHomePage = {
-        // backgroundColor: "red" 
-    };
-  
-    // console.log(config.playlists);
-  
+function HomePage(){
+  const estilosDaHomePage = {
+
+  }
+
+  const [valorDoFilter, setValorDoFilter] = React.useState("");
     return (
         <>
             <CSSReset />
             <div style={{
                 display: "flex",
                 flexDirection: "column",
-                flex: 1,
-                backgroundColor: "red",
+                flex: 1
+                
             }}>
-                <Menu />
+                <Menu valorDoFilter={valorDoFilter} setValorDoFilter={setValorDoFilter}/>
                 <Header />
-                <TimeLine playlists={config.playlists}>
-                    Conteúdo
-                </TimeLine>
+                
+                <Timeline searchValue={valorDoFilter} playlists={config.playlists}>
+                  Conteudo
+                </Timeline>
             </div>
         </>
     );
-  }
-  
+}
+export default HomePage
 
 //function Menu(){
 //    return (
@@ -45,7 +46,7 @@ const StylecHeader = styled.div`
   }
 
   .user-info {
-    margin-top: 50px;
+    background-image: url(${config.bg});
     display: flex;
     align-items: center;
     width: 100%;
@@ -54,9 +55,15 @@ const StylecHeader = styled.div`
   }
 `;
 
+const StyledBanner = styled.div`
+  background-image: blue;
+  background-image: url(${({ bg })} => bg)};
+  hight: 230px
+`;
 function Header() {
   return (
     <StylecHeader>
+    <StyledBanner bg={bg} />
       {/*<img src="banner" />*/}
       <section className="user-info">
         <img src={`https://github.com/${config.github}.png`} />
@@ -70,31 +77,39 @@ function Header() {
   );
 }
 
-// function TimeLine(propriedades) {
-//   console.log("Dentro do componente", propriedades.playlists);
-//   const playlistNames = Object.keys(propriedades.playlists);
-//   return (
-//     <div>
-//       {playlistNames.map((playlistNames) => {
-//         const videos = propriedades.playlists[playlistNames];
-
-//         return (
-//           <section>
-//             <h2>{playlistNames}</h2>
-//             <div>
-//               {videos.map((video) => {
-//                 return (
-//                   <a href={video.url}>
-//                     <img src={video.thumb} />
-//                     <span>{video.title}</span>
-//                   </a>
-//                 );
-//               })}
-//             </div>
-//           </section>
-//         );
-//       })}
-//     </div>
-//   );
-// }
+function Timeline({searchValue, ...propriedades}) {
+  //console.log("Dentro do componente", propriedades.playlists);
+  const playlistNames = Object.keys(propriedades.playlists);
+  return (
+    
+    <StyledTimeline>
+      {playlistNames.map((playlistNames) => {
+        const videos = propriedades.playlists[playlistNames];
+        console.log(playlistNames);
+        console.log(videos);
+        return (
+          <section key={playlistNames}>
+            <h2>{playlistNames}</h2>
+            <div>
+              {videos.filter((video) => {
+                const titleNormalized = video.title.toLowerCase();
+                const searchValueNormalized = video.title.toLowerCase();
+                return titleNormalized.includes(searchValueNormalized)
+              }).map((video) => {
+                return (
+                  <a key={video.url} href={video.url}>
+                    <img src={video.thumb} />
+                    <span>{video.title}</span>
+                  </a>
+                )
+              })}
+            </div>
+          </section>
+        );
+      })}
+    
+    <StyledTimeline />
+    
+  );
+}
 
--- src/componets/Menu/index.js
+++ src/componets/Menu/index.js
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import Search from "./components/Search";
+
 
 const StyledMenu = styled.header`
   display: flex;
@@ -24,12 +26,13 @@ const StyledMenu = styled.header`
   }
 `;
 
-export default function Menu() {
+export default function Menu(valorDoFilter, setValorDoFilter) {
   return (
     <StyledMenu>
       <div>
         <Logo />
       </div>
+        <Search valorDoFilter={valorDoFilter} setValorDoFilter={setValorDoFilter}/>
     </StyledMenu>
   );
 }
